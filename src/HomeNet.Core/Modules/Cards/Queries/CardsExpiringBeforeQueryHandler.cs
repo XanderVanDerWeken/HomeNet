8fc23e5a021cc4b976bbd7f7548f7d42f5277fdd
xa3687va <xander.van.der.weken@fh-erfurt.de>
2025-12-06 15:48:59 +0100

    Adding Card Queries

---
+++ src/HomeNet.Core/Modules/Cards/Queries/CardsExpiringBeforeQueryHandler.cs
@@ -0,0 +1,25 @@
+using HomeNet.Core.Common;
+using HomeNet.Core.Common.Cqrs;
+using HomeNet.Core.Modules.Cards.Abstractions;
+using HomeNet.Core.Modules.Cards.Models;
+
+namespace HomeNet.Core.Modules.Cards.Queries;
+
+public sealed class CardsExpiringBeforeQueryHandler : IQueryHandler<CardsExpiringBeforeQuery, IReadOnlyList<Card>>
+{
+    private readonly ICardRepository _cardRepository;
+
+    public CardsExpiringBeforeQueryHandler(ICardRepository cardRepository)
+    {
+        _cardRepository = cardRepository;
+    }
+
+    public async Task<Result<IReadOnlyList<Card>>> HandleAsync(
+        CardsExpiringBeforeQuery query, 
+        CancellationToken cancellationToken = default)
+    {
+        var cards = await _cardRepository.GetAllCardsWithExpiryBeforeAsync(query.ExpiryDate, cancellationToken);
+
+        return Result<IReadOnlyList<Card>>.Success(cards);
+    }
+}
