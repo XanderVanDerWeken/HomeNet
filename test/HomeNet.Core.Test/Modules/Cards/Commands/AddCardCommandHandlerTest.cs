7c732a83194e3cc9292287e6dd07243610d1664c
xa3687va <xander.van.der.weken@fh-erfurt.de>
2025-12-06 22:13:50 +0100

    Adding Command and Query Tests

---
+++ test/HomeNet.Core.Test/Modules/Cards/Commands/AddCardCommandHandlerTest.cs
@@ -0,0 +1,94 @@
+using HomeNet.Core.Modules.Cards.Abstractions;
+using HomeNet.Core.Modules.Cards.Commands;
+using HomeNet.Core.Modules.Cards.Models;
+using Moq;
+
+namespace HomeNet.Core.Test.Modules.Cards.Commands;
+
+public class AddCardCommandHandlerTest
+{
+    private AddCardCommandHandler _handler;
+
+    private Mock<ICardRepository> _cardRepositoryMock;
+
+    [SetUp]
+    public void Setup()
+    {
+        _cardRepositoryMock = new Mock<ICardRepository>();
+
+        _handler = new AddCardCommandHandler(_cardRepositoryMock.Object);
+    }
+
+    [Test]
+    public async Task HandleAsync_ShouldReturnSuccess()
+    {
+        // Arrange
+        var command = new AddCardCommand
+        {
+            Name = "Test Card",
+            ExpirationDate = DateTime.UtcNow.AddDays(7),
+        };
+
+        _cardRepositoryMock
+            .Setup(r => r.AddCardAsync(It.IsAny<Card>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Core.Common.Result.Success());
+
+        // Act
+        var result = await _handler.HandleAsync(command);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.IsSuccess, Is.True);
+            Assert.That(result.Error, Is.Null);
+        });
+
+        _cardRepositoryMock.Verify(
+            r => r.AddCardAsync(
+                It.Is<Card>(c =>
+                    c.Name == command.Name &&
+                    c.ExpirationDate == command.ExpirationDate),
+                It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+
+    [Test]
+    public async Task HandleAsync_ShouldReturnFailure_InvalidCommand()
+    {
+        // Arrange
+        var commandInvalidName = new AddCardCommand
+        {
+            Name = string.Empty,
+            ExpirationDate = DateTime.UtcNow,
+        };
+        var commandInvalidDate = new AddCardCommand
+        {
+            Name = "Valid Name",
+            ExpirationDate = DateTime.UtcNow.AddMonths(-6),
+        };
+
+        _cardRepositoryMock
+            .Setup(r => r.AddCardAsync(It.IsAny<Card>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Core.Common.Result.Success());
+
+        // Act
+        var resultInvalidName = await _handler.HandleAsync(commandInvalidName);
+        var resultInvalidDate = await _handler.HandleAsync(commandInvalidDate);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(resultInvalidName.IsSuccess, Is.False);
+            Assert.That(resultInvalidName.Error, Is.Not.Null);
+
+            Assert.That(resultInvalidDate.IsSuccess, Is.False);
+            Assert.That(resultInvalidDate.Error, Is.Not.Null);
+        });
+
+        _cardRepositoryMock.Verify(
+            r => r.AddCardAsync(
+                It.IsAny<Card>(), 
+                It.IsAny<CancellationToken>()), 
+            Times.Never);
+    }
+}
---
+++ test/HomeNet.Core.Test/Modules/Cards/Queries/CardsQueryHandlerTest.cs
@@ -0,0 +1,57 @@
+using System.Threading.Tasks;
+using HomeNet.Core.Modules.Cards.Abstractions;
+using HomeNet.Core.Modules.Cards.Models;
+using HomeNet.Core.Modules.Cards.Queries;
+using Moq;
+using NUnit.Framework;
+
+namespace HomeNet.Core.Test.Modules.Cards.Queries;
+
+public class CardsQueryHandlerTest
+{
+    private CardsQueryHandler _handler;
+
+    private Mock<ICardRepository> _cardRepositoryMock;
+
+    [SetUp]
+    public void Setup()
+    {
+        _cardRepositoryMock = new Mock<ICardRepository>();
+
+        _handler = new CardsQueryHandler(_cardRepositoryMock.Object);
+    }
+
+    [Test]
+    public async Task HandleAsync_ShouldReturnCards()
+    {
+        // Arrange
+        var query = new CardsQuery();
+
+        var card = new Card
+        {
+            Id = 1,
+            Name = "Test Card",
+            ExpirationDate = DateTimeOffset.UtcNow.AddDays(14),
+        };
+
+        _cardRepositoryMock
+            .Setup(x => x.GetAllCardsAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new[] { card });
+
+        // Act
+        var result = await _handler.HandleAsync(query);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.IsSuccess, Is.True);
+            Assert.That(result.Value, Has.Count.EqualTo(1));
+            Assert.That(result.Value![0], Is.EqualTo(card));
+        });
+
+        _cardRepositoryMock.Verify(
+            r => r.GetAllCardsAsync(
+                It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+}
