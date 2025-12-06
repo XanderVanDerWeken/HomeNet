4b7da5ce9cc06d4a08a4b9d55a67b9e244499aa9
xa3687va <xander.van.der.weken@fh-erfurt.de>
2025-12-06 15:48:39 +0100

    Updating Cards Core

--- src/HomeNet.Core/Modules/Cards/Abstractions/ICardRepository.cs
+++ src/HomeNet.Core/Modules/Cards/Abstractions/ICardRepository.cs
@@ -1,6 +1,22 @@
+using HomeNet.Core.Common;
+using HomeNet.Core.Modules.Cards.Models;
+
 namespace HomeNet.Core.Modules.Cards.Abstractions;
 
 public interface ICardRepository
 {
+    Task<Result> AddCardAsync(
+        Card card, 
+        CancellationToken cancellationToken = default);
+    
+    Task<Result> RemoveCardAsync(
+        int cardId, 
+        CancellationToken cancellationToken = default);
 
+    Task<IReadOnlyList<Card>> GetAllCardsAsync(
+        CancellationToken cancellationToken = default);
+    
+    Task<IReadOnlyList<Card>> GetAllCardsWithExpiryBeforeAsync(
+        DateTimeOffset expiryDate, 
+        CancellationToken cancellationToken = default);
 }
