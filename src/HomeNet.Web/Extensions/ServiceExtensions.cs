15d05ed972e55c53a07cc2decb29c2fca97d1c28
xa3687va <xander.van.der.weken@fh-erfurt.de>
2025-12-06 20:36:44 +0100

    Adding DB in DI

---
+++ src/HomeNet.Web/Extensions/ServiceExtensions.cs
@@ -0,0 +1,34 @@
+using HomeNet.Core.Common.Cqrs;
+using Npgsql;
+using SqlKata.Compilers;
+using SqlKata.Execution;
+
+namespace HomeNet.Web.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddCommonServices(
+        this IServiceCollection services)
+    {
+        services.AddSingleton<IMediator, EventBus>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddDatabase(
+        this IServiceCollection services, 
+        IConfiguration config)
+    {
+        services.AddSingleton<QueryFactory>(sp =>
+        {
+            var connectionString = config.GetConnectionString("Default");
+
+            var connection = new NpgsqlConnection(connectionString);
+            var compiler = new PostgresCompiler();
+
+            return new QueryFactory(connection, compiler);
+        });
+
+        return services;
+    }
+}
