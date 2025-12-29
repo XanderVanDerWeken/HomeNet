48566604df264e9157fe4b1531fca34cf1d15d83
XanderVanDerWeken <vanxa812@yahoo.de>
2025-12-29 10:54:27 +0100

    Adding AddPersonCommand

---
+++ src/HomeNet.Core/Modules/Persons/Commands/AddPersonCommandHandler.cs
@@ -0,0 +1,38 @@
+using HomeNet.Core.Common;
+using HomeNet.Core.Common.Cqrs;
+using HomeNet.Core.Modules.Persons.Abstractions;
+using HomeNet.Core.Modules.Persons.Models;
+
+namespace HomeNet.Core.Modules.Persons.Commands;
+
+public sealed class AddPersonCommandHandler : ICommandHandler<AddPersonCommand>
+{
+    private readonly IPersonRepository _personRepository;
+
+    public AddPersonCommandHandler(IPersonRepository personRepository)
+    {
+        _personRepository = personRepository;
+    }
+
+    public Task<Result> HandleAsync(
+        AddPersonCommand command, 
+        CancellationToken cancellationToken = default)
+    {
+        var validationResult = command.Validate();
+
+        if (!validationResult.IsValid)
+        {
+            var result = Result.Failure(validationResult.ErrorMessage!);
+            return Task.FromResult(result);
+        }
+
+        var newPerson = new Person
+        {
+            FirstName = command.FirstName,
+            LastName = command.LastName,
+            AliasName = command.AliasName,
+        };
+        
+        return _personRepository.AddPersonAsync(newPerson, cancellationToken);
+    }
+}
