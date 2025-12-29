4ff12a7dfe126d1659cdbef32ff0f5d3662aade3
XanderVanDerWeken <vanxa812@yahoo.de>
2025-12-29 11:12:16 +0100

    Adding Persons Schema to SQL Script

--- sql/postgres.initdb.sql
+++ sql/postgres.initdb.sql
@@ -1,5 +1,14 @@
 BEGIN;
 
+CREATE SCHEMA IF NOT EXISTS persons;
+
+CREATE TABLE IF NOT EXISTS persons.persons (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    alias_name VARCHAR(100) NULL
+);
+
 CREATE SCHEMA IF NOT EXISTS cards;
 
 CREATE TABLE IF NOT EXISTS cards.cards (
