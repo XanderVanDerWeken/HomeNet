62227c0d75b4af3604bd772e09e54236fe5b59ba
XanderVanDerWeken <vanxa812@yahoo.de>
2025-12-29 10:54:22 +0100

    Adding simple Person Abstractions

---
+++ src/HomeNet.Core/Modules/Persons/Abstractions/IPersonRepository.cs
@@ -0,0 +1,14 @@
+using HomeNet.Core.Common;
+using HomeNet.Core.Modules.Persons.Models;
+
+namespace HomeNet.Core.Modules.Persons.Abstractions;
+
+public interface IPersonRepository
+{
+    Task<Result> AddPersonAsync(
+        Person person, 
+        CancellationToken cancellationToken = default);
+    
+    Task<IReadOnlyList<Person>> GetAllPersonsAsync(
+        CancellationToken cancellationToken = default);
+}
