eb7d2e27686c27d85bdbe19254a5819540052d3a
XanderVanDerWeken <vanxa812@yahoo.de>
2025-12-31 14:21:56 +0100

    Working on Upsert MonthlyTimeline in cache

--- sql/cache.initdb.sql
+++ sql/cache.initdb.sql
@@ -3,5 +3,7 @@ CREATE TABLE IF NOT EXISTS monthly_timelines (
     month INTEGER NOT NULL,
     income_amount NUMERIC(12, 2) NOT NULL,
     expense_amount NUMERIC(12, 2) NOT NULL,
-    net_amount NUMERIC(12, 2) NOT NULL
+    net_amount NUMERIC(12, 2) NOT NULL,
+
+    UNIQUE (year, month)
 );
