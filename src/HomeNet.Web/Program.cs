30a13c95d7290d52f538dedaa971d67900d3cdb0
XanderVanDerWeken <vanxa812@yahoo.de>
2026-02-06 22:27:33 +0100

    Adding simple Login

--- src/HomeNet.Web/Program.cs
+++ src/HomeNet.Web/Program.cs
@@ -1,8 +1,14 @@
+using System.Security.Claims;
+using HomeNet.Core.Common.Cqrs;
+using HomeNet.Core.Modules.Auth.Queries;
 using HomeNet.Web.Components;
 using HomeNet.Web.Configurations;
 using HomeNet.Web.Database;
 using HomeNet.Web.Extensions;
+using HomeNet.Web.Models;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.Extensions.Options;
 using MudBlazor.Services;
 using Serilog;
@@ -44,12 +50,14 @@ public class Program
         });
 
         // Authentication and Authorization
+        builder.Services.AddHttpContextAccessor();
+
         builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
             .AddCookie(options =>
             {
                 options.LoginPath = "/login";
                 options.LogoutPath = "/logout";
-                options.AccessDeniedPath = "/access-denied";
+                options.AccessDeniedPath = "/forbidden";
                 options.ExpireTimeSpan = TimeSpan.FromHours(8);
                 options.SlidingExpiration = true;
             });
@@ -78,6 +86,44 @@ public class Program
         app.MapStaticAssets();
         app.MapRazorComponents<App>()
             .AddInteractiveServerRenderMode();
+
+        app.MapPost("/api/auth/login", async (HttpContext context, HttpRequest request, IEventBus bus) =>
+        {
+            var query = new UserWithCredentialsQuery
+            {
+                UserName = request.Form["username"]!,
+                Password = request.Form["password"]!,
+            };
+
+            var queryResult = await bus.SendAsync<bool>(query);
+
+            if (queryResult.IsSuccess && queryResult.Value)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, query.UserName),
+                    new Claim(ClaimTypes.Role, "User"), // TODO: Query user role
+                };
+
+                var identity = new ClaimsIdentity(
+                    claims, 
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+                
+                await context.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme, 
+                    new ClaimsPrincipal(identity));
+                
+                return Results.Redirect("/");
+            }
+
+            return Results.Unauthorized();
+        });
+
+        app.MapPost("/api/auth/logout", async (HttpContext context) =>
+        {
+            await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Results.Ok();
+        });
 
         using (var scope = app.Services.CreateScope())
         {
