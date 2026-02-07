aab0e736defcdfef79e7e9e5488c41091f83c281
XanderVanDerWeken <vanxa812@yahoo.de>
2026-02-07 16:30:38 +0100

    Adding Wiring up

--- src/HomeNet.Web/Extensions/ServiceExtensions.cs
+++ src/HomeNet.Web/Extensions/ServiceExtensions.cs
@@ -1,10 +1,26 @@
 using HomeNet.Core.Common.Events;
+using HomeNet.Core.Modules.Auth.Abstractions;
+using HomeNet.Core.Modules.Auth.Commands;
+using HomeNet.Core.Modules.Auth.Queries;
 using HomeNet.Core.Modules.Cards.Abstractions;
 using HomeNet.Core.Modules.Cards.Commands;
 using HomeNet.Core.Modules.Cards.Queries;
+using HomeNet.Core.Modules.Finances.Abstractions;
+using HomeNet.Core.Modules.Finances.Commands;
+using HomeNet.Core.Modules.Finances.Queries;
+using HomeNet.Core.Modules.Finances.Services;
+using HomeNet.Core.Modules.Persons.Abstractions;
+using HomeNet.Core.Modules.Persons.Commands;
+using HomeNet.Core.Modules.Persons.Queries;
+using HomeNet.Infrastructure.Cache.Modules.Finances;
 using HomeNet.Infrastructure.Events;
 using HomeNet.Infrastructure.Persistence.Abstractions;
+using HomeNet.Infrastructure.Persistence.Modules.Auth;
 using HomeNet.Infrastructure.Persistence.Modules.Cards;
+using HomeNet.Infrastructure.Persistence.Modules.Finances;
+using HomeNet.Infrastructure.Persistence.Modules.Persons;
+using HomeNet.Infrastructure.Security;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Npgsql;
 using SqlKata.Compilers;
 
@@ -36,11 +52,79 @@ public static class ServiceExtensions
     {
         services.AddScoped<ICardRepository, CardRepository>();
 
-        services.AddScoped<AddCardCommandHandler>();
-        services.AddScoped<RemoveCardCommandHandler>();
+        services
+            .AddTransient<AddCardCommandHandler>()
+            .AddTransient<RemoveCardCommandHandler>();
 
-        services.AddScoped<CardsQueryHandler>();
-        services.AddScoped<CardsExpiringBeforeQueryHandler>();
+        services
+            .AddTransient<CardsQueryHandler>()
+            .AddTransient<CardsExpiringBeforeQueryHandler>();
+
+        // TODO: Register Handlers with EventBus
+
+        return services;
+    }
+
+    public static IServiceCollection AddAuthModule(
+        this IServiceCollection services)
+    {
+        services
+            .AddScoped<IPasswordService, PasswordService>()
+            .AddScoped<IUserRepository, UserRepository>();
+        services.TryAddScoped<IPersonRepository, PersonRepository>();
+
+        services
+            .AddTransient<AddUserCommandHandler>()
+            .AddTransient<LinkPersonToUserCommandHandler>()
+            .AddTransient<UnlinkPersonFromUserCommandHandler>();
+        services.TryAddScoped<IPersonRepository, PersonRepository>();
+
+        services.AddTransient<UserWithCredentialsQueryHandler>();
+
+        // TODO: Register Handlers with EventBus
+
+        return services;
+    }
+
+    public static IServiceCollection AddFinancesModule(
+        this IServiceCollection services)
+    {
+        services
+            .AddScoped<ICategoryRepository, CategoryRepository>()
+            .AddScoped<ITransactionRepository, TransactionRepository>();
+        services.AddSingleton<IMonthlyTimelineRepository, MonthlyTimelineCache>();
+
+        services.AddScoped<ITimelineBuilder, TimelineBuilder>();
+
+        services
+            .AddTransient<AddCategoryCommandHandler>()
+            .AddTransient<AddExpenseCommandHandler>()
+            .AddTransient<AddIncomeCommandHandler>();
+        
+        services
+            .AddTransient<CategoriesQueryHandler>()
+            .AddTransient<ExpensesQueryHandler>()
+            .AddTransient<IncomesQueryHandler>()
+            .AddTransient<MonthlyTimelineQueryHandler>();
+
+        // TODO: Register Handlers with EventBus
+
+        return services;
+    }
+
+    public static IServiceCollection AddPersonsModule(
+        this IServiceCollection services)
+    {
+        services.TryAddScoped<IPersonRepository, PersonRepository>();
+
+        services
+            .AddTransient<AddPersonCommandHandler>()
+            .AddTransient<UpdatePersonCommandHandler>();
+        
+        services
+            .AddTransient<PersonsQueryHandler>();
+
+        // TODO: Register Handlers with EventBus
 
         return services;
     }
