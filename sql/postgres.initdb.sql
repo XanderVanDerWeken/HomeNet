bbdc91819a8d12a45480615e34dbb1facf54feed
xa3687va <xander.van.der.weken@fh-erfurt.de>
2026-02-21 19:30:24 +0100

    Updating DB Script

--- sql/postgres.initdb.sql
+++ sql/postgres.initdb.sql
@@ -32,4 +32,42 @@ CREATE TABLE IF NOT EXISTS cards.cards (
 
 CREATE SCHEMA IF NOT EXISTS finances;
 
+CREATE TABLE IF NOT EXISTS finances.categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS finances.transactions(
+    id SERIAL PRIMARY KEY,
+    category_id INT REFERENCES finances.categories(id),
+    amount DECIMAL(18, 2) NOT NULL CHECK (amount > 0),
+    type VARCHAR(10) NOT NULL CHECK (type IN ('Income', 'Expense')),
+    source VARCHAR(20) NOT NULL DEFAULT 'Manual' CHECK (source IN ('Manual', 'FixedCost')),
+    description TEXT NULL,
+    date DATE NOT NULL
+);
+
+CREATE INDEX idx_transactions_date ON finances.transactions(date);
+CREATE INDEX idx_transactions_category_id ON finances.transactions(category_id);
+
+CREATE TABLE IF NOT EXISTS finances.fixed_costs (
+    id SERIAL PRIMARY KEY,
+    category_id INT REFERENCES finances.categories(id),
+    name VARCHAR(100) NOT NULL,
+    day_of_month INT NOT NULL CHECK (day_of_month BETWEEN 1 AND 28),
+);
+
+CREATE TABLE IF NOT EXISTS finances.fixed_cost_versions (
+    id SERIAL PRIMARY KEY,
+    fixed_cost_id INT REFERENCES finances.fixed_costs(id),
+    amount DECIMAL(18, 2) NOT NULL CHECK (amount > 0),
+    valid_from DATE NOT NULL,
+    valid_to DATE NULL,
+
+    CONSTRAINT valid_range CHECK (valid_to IS NULL OR valid_to > valid_from
+);
+
+CREATE INDEX idx_fixed_cost_versions_fixed_cost_id ON finances.fixed_cost_versions(fixed_cost_id);
+CREATE INDEX idx_fixed_cost_versions_valid_range ON finances.fixed_cost_versions(fixed_cost_id, valid_from, valid_to);
+
 COMMIT;
